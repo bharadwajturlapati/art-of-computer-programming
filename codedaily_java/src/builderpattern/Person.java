d38fa3785fa68b0406325556a77480a0e3ad7d30
bharadwaj.turlapati <tbj.name@gmail.com>
2017-03-23 20:47:24 +0530

    Person Builder Pattern

---
+++ codedaily_java/src/builderpattern/Person.java
@@ -0,0 +1,49 @@
+package builderpattern;
+
+public class Person {
+	//Required
+	private int age;
+	private String phonenumber;
+	private String Address;
+	private Double salary;
+	//Required
+	private String email;
+	//Required password
+	private String password;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}

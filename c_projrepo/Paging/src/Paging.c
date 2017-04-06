effe185392820672dffbbcc80706edc9d4fb9c8a
bharadwaj.turlapati <tbj.name@gmail.com>
2017-04-06 09:06:04 +0530

    committing paging and worker threads

---
+++ c_projrepo/Paging/src/Paging.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Paging.c
+ Author      : bharadwaj_turlapati
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int Main() {
+	puts("hello world"); /* prints hello world */
+	return EXIT_SUCCESS;
+}
---
+++ c_projrepo/Paging/src/SimVirtualAddress.c
@@ -0,0 +1,34 @@
+/*
+ * SimVirtualAddress.c
+ *
+ *  Created on: Apr 6, 2017
+ *      Author: bharadwaj_turlapati
+ */
+
+/* Author : Teja Sasank Gorthi
+ Email  : jet.sasank@gmail.com */
+
+/*Problem : Assume that a system has a 32-bit virtual address with a 4-KB page size.
+ Write a C program that is passed a virtual address (in decimal) on the command line and have it output the page number and offset for the given address.*/
+
+#include <stdio.h>
+
+int main() {
+	unsigned long page;
+	unsigned long offset;
+	unsigned long address;
+
+	unsigned long getInput;
+	address = atoll(getInput);
+	/* Page Number =  quotient of  address / 4KB and offset = remainder*/
+	/*Below is the faster method of calculating the same*/
+
+	page = address >> 12; /* Since page size is 4KB => 12 bits holding the virtual address*/
+	offset = address & 0xfff;
+
+	printf("The address %lu contains: \n", address);
+	printf("page number = %lu\n", page);
+	printf("offset = %lu\n", offset);
+
+	return 0;
+}

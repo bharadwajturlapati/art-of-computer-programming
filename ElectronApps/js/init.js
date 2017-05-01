784cb2fcd08e89fcecad22d9bc5cedd3592eff12
bharadwaj.turlapati <tbj.name@gmail.com>
2017-05-01 11:51:08 +0530

    introducing angular JS

--- ElectronApps/js/init.js
+++ ElectronApps/js/init.js
@@ -1,19 +1,19 @@
 var $ts = {
-    preinit : function() {
-	    // Register Jquery
-	    window.jQuery = window.$ = require('jquery');
-    },
-    isServerHealthy : function() {
-	    $networkUtils.ajax($staticModels.healthCheckURLConfig);
-    },
-    init : function() {
-	    $ts.isServerHealthy();
-	    $registerEvents.initEvents();
-	    // Pre cache Parent Nodes in a global variable for the other operations to work
-	    $genericViews.loadIdNodesFromUI();
-	    $staticModels.createStaticDomNodesCache();
-	    //$view.loadSplashScreen();
-	    $controller.loadAllData();
-	    $view.dismissSplash();
-    }
+	preinit : function() {
+		// Register Jquery
+		window.jQuery = window.$ = require('jquery');
+	},
+	isServerHealthy : function() {
+		$networkUtils.ajax($staticModels.healthCheckURLConfig);
+	},
+	init : function() {
+		$ts.isServerHealthy();
+		$registerEvents.initEvents();
+		// Pre cache Parent Nodes in a global variable for the other operations to work
+		$genericViews.loadIdNodesFromUI();
+		$staticModels.createStaticDomNodesCache();
+		//$view.loadSplashScreen();
+		$controller.loadAllData();
+		$view.dismissSplash();
+	}
 }

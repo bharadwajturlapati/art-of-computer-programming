3d4a9a857af4e3c4e2b01c31127752249dd6b918
bharadwaj.turlapati <tbj.name@gmail.com>
2018-02-19 10:36:02 +0530

    introducing standalone fabric

--- WSF/js/splash/splash.js
+++ WSF/js/splash/splash.js
@@ -1,9 +1,31 @@
+var fs = require('fs');
+
 function showMainPage(){
 	window.location = "main.html"
 }
 
+function loadproperties(){
+	loadDBProperties();
+}
+
 function setTimeOutToReleaseSplash(){
+	loadproperties();
 	setTimeout(function(){
 		showMainPage();
 	}, 3000);
+}
+
+function loadDBProperties(){
+	var lineReaderObj = getLineReader('wsf.pref');
+	lineReaderObj.on('line', function (lineStr) {
+  		var propsArray = lineStr.split('=');
+  		localStorage.setItem(propsArray[0], propsArray[1]);
+	});
+}
+
+function getLineReader(file){
+	var lineReader = require('readline').createInterface({
+  		input: require('fs').createReadStream(file)
+	});
+	return lineReader;
 }
